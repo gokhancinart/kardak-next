3f9a1ecf3a0dff379cbe4f5700db66f3e1c54f1f
Gökhan <gkn.cinar@gmail.com>
2025-03-08 18:07:22 +0300

    site2 url removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ export default function Home() {
 
   console.log('Env Check:', {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
-    siteUrl2: SITE_URL2,
     locale: router.locale,
     path: router.asPath
   })
