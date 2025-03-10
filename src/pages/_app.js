2fa3da87bccbb31804451eb7975a1eb3daea879c
gokhancinart <gkn.cinar@gmail.com>
2025-03-10 22:12:03 +0300

    analystic fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,13 @@ import { appWithTranslation } from 'next-i18next';
 import Layout from '../../components/Layout';
 import '../styles/globals.css';
 
+import { Analytics } from "@vercel/analytics/react"
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
